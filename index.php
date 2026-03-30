8378bc6e55f55eb0c1c3d6ee2de586e02f703ba6
Digvijay Sunil Gogawale <158759860+digvijay-0406@users.noreply.github.com>
2026-03-30 14:52:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$conn = new mysqli("localhost", "root", "barsaat", "login");
+$conn = new mysqli("localhost", "root", "******", "login");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
